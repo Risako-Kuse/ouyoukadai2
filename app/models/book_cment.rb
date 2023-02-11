845a1a0ce4b90e2e599ef30b85746e539581b1d1
EC2 Default User <ec2-user@ip-172-31-6-204.ap-northeast-1.compute.internal>
2023-02-11 13:44:51 +0000

    2023021102

--- app/models/book_cment.rb
+++ app/models/book_cment.rb
@@ -2,6 +2,22 @@ class BookCment < ApplicationRecord
 
   # UserモデルとBookCommentモデルを関連付ける
   belongs_to :user
+  validates :title,presence:true
+	validates :body,presence:true,length:{maximum:200}
+
+  # 検索方法分岐
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
+
   belongs_to :book
 
   # コメントモデルのバリデーション設定
